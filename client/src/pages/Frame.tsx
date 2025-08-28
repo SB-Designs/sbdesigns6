067a039396089a8cfcf0bcecdf45564859912272
SB Designs <hello@sbdesigns.spike.team>
2025-08-28 11:47:06 +0100

    Update Frame.tsx

--- client/src/pages/Frame.tsx
+++ client/src/pages/Frame.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/ui/button";
 const navigationItems = [
   { label: "Projects", href: "#projects" },
   { label: "Subsidiaries", href: "#subsidiaries" },
+  { label: "Docs", href: "https://sb-designs.gitbook.io" },
 ];
 
 const projectsData = [
