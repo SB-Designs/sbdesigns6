fd2318ee20d02bd8306c293410af6da86949c3e3
Sebastian <hello@sbdesigns.is-a.dev>
2026-03-30 12:39:53 +0100

    Remove Media import and media route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,6 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
-import { Frame as Media } from "@/pages/Media";
 
 import { Frame } from "@/pages/Frame";
 
@@ -13,7 +12,6 @@ function Router() {
     <Switch>
       {/* Add pages below */}
       <Route path="/" component={Frame} />
-      <Route path="/media" component={Media} />
       {/* Fallback to 404 */}
       <Route component={NotFound} />
     </Switch>
